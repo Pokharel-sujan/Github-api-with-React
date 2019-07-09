2ac8cd428ed83b518d38805ab1953a38aa34e559
Sujan <pokharelsujan134@gmail.com>
2019-07-09 15:10:48 +0300

    github profile

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import SearchBox from '../src/components/SearchBox';
+import Card from '../src/components/Card';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      username: 'Pokharel-sujan',
+      realName: '',
+      avatar: '',
+      location: '',
+      repos: '',
+      followers: '',
+      url: '',
+      notFound: ''
+    }
+  }
+  render() {
+    return (
+      <div>
+        <SearchBox fetchUser={this.fetchUser.bind(this)}/>
+        <Card data={this.state} />
+      </div>
+    )
+  }
+  
+  // the api request function
+  fetchApi(url) { 
+    
+    fetch(url)
+      .then((res) => res.json() )
+      .then((data) => {
+        
+        // update state with API data
+        this.setState({
+          username: data.login,
+          realName: data.name,
+          avatar: data.avatar_url,
+          location: data.location,
+          repos: data.public_repos,
+          followers: data.followers,
+          url: data.html_url,
+          notFound: data.message
+        })
+      })
+      .catch((err) => console.log('oh no!') )
+  }
+  
+  fetchUser(username) {
+    let url = `https://api.github.com/users/${username}`
+    this.fetchApi(url)
+  }
+  
+  componentDidMount() {
+    let url = `https://api.github.com/users/${this.state.username}`
+    this.fetchApi(url)
+  }
 }
 
 export default App;
---
+++ src/components/SearchBox.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+class SearchBox extends Component {
+    render() {
+      return (
+        <form 
+          className="searchbox" 
+          onSubmit={this.handleClick.bind(this)}>
+          <input
+            ref="search"
+            className="searchbox__input" 
+            type="text" 
+            placeholder="type username..."/>
+          
+          <input
+            type="submit"
+            className="searchbox__button"
+            value="Search GitHub User" />
+        </form>
+      )
+    };
+    handleClick(e) {
+        e.preventDefault();
+        
+        let username = this.refs.search.value
+        // sending the username value to parent component to fetch new data from API
+        this.props.fetchUser(username)
+        this.refs.search.value = ''
+      }
+    }
+    export default SearchBox;
+    
